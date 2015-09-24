eee2e2aafa467e08e8cf8d7491e01a352d6d1078
Amit <dragnoth46@gmail.com>
2015-09-25 02:58:06 +0800

    Visual Keywords + Color Hist Combined

--- app/imageEngine/ImageComparator.py
+++ app/imageEngine/ImageComparator.py
@@ -17,22 +17,36 @@ class ImageComparator:
 		otherHist = np.asarray(otherHist)
 		return np.linalg.norm(queryHist - otherHist)
 
-	def compareAgainstDB(self, queryFeatures, limit = 10):
+	def visualDistance(self, queryHist, otherHist):
+		otherHist = np.asarray(otherHist)
+		return dist.cosine(queryHist, otherHist)
+
+	def compareAgainstDB(self, queryFeatures, flag, limit = 20):
 		# initialize our dictionary of results
 		results = {}
 
 		with open(self.indexPath) as imgInfo:
-			simplejson.load(imgInfo)
+			queryDict = simplejson.load(imgInfo)
 
 		# queryDict = simplejson.loads(open(self.indexPath, "rb"))
-
 		for imgName, info in queryDict.iteritems():
-			features = info["histoDetails"]
-			distance = self.euclideanDistance(queryFeatures, features)
-			results[imgName] = distance
+			
+			if flag == "color":
+				#Color Hist Comparsion
+				features = info["histoDetails"]
+				distance = self.euclideanDistance(queryFeatures, features) 
+				results[imgName] = distance
+			elif flag == "visual":
+				#Visual Hist
+				visualHist = info["visualHist"]
+				# print "EDMUND:", len(visualHist)
+				distance = self.visualDistance(queryFeatures, visualHist)
+				results[imgName] = distance
 
-		results = sorted([(v, k) for (k, v) in results.items()])
-		return results[:limit]
+		print "DING", len(results)
+		return results
+		# results = sorted([(v, k) for (k, v) in results.items()])
+		# return results[:limit]
 		# return results
 
 	def distIntersection(self, queryHist, otherHist):
@@ -41,6 +55,30 @@ class ImageComparator:
 
 		return cv2.compareHist(queryHist, otherHist, cv2.cv.CV_COMP_INTERSECT)
 
+	@np.vectorize
+	def weightResults(color, visualKey):
+		colorWeight = 0.05
+		visualKeyWeight = 0.95
+
+		return (color * colorWeight) + (visualKey * visualKeyWeight)
+
+	def combineResults(self, color, visualKey, limit = 20):
+		keys = color.keys()
+		print "METALLICA"
+		if color.keys() == visualKey.keys():
+			print "HALLE"
+
+		color = np.asarray(color.values())
+		visualKey = np.asarray(visualKey.values())
+
+		newVals = self.weightResults(color, visualKey)
+
+		weightedScores = dict(zip(keys, newVals.tolist()))
+
+		rankedScores = sorted([(v, k) for (k, v) in weightedScores.items()])
+
+		print rankedScores[:limit]
+		return rankedScores[:limit]
 
 	# def chi2_distance(self, histA, histB, eps = 1e-10):
 	# 	histA = np.asarray(histA)
--- app/imageEngine/QueryAnalyzer.py
+++ app/imageEngine/QueryAnalyzer.py
@@ -1,14 +1,24 @@
 import os
 import cv2
 from imageEngine.ColorDescriptor import ColorDescriptor
+from imageEngine.BoW.TrainModel import TrainModel
 
 class QueryAnalyzer:
     def __init__(self, queryPath):
+    	print "\nanalysing\n"
         self.queryPath = queryPath
 
-    def analyze(self):
-        print "\nanalysing\n"
-        queryImage = cv2.imread(self.queryPath)
-        cd = ColorDescriptor((8, 12, 3))
-        queryHist = cd.extractHist(queryImage)
-        return queryHist
+    def analyze(self, flag):
+    	# print "\nanalysing\n"
+		if flag == "color":
+			queryImage = cv2.imread(self.queryPath)
+			cd = ColorDescriptor((8, 12, 3))
+			queryHist = cd.extractHist(queryImage)
+		elif flag == "visual":
+			uploadLocation =  "static/upload/"
+			# print uploadLocation
+			t = TrainModel(uploadLocation, "analyze")
+			queryHist = t.uploadAnalysis()
+		
+		# print "query", queryHist
+		return queryHist
